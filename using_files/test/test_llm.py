17923d729e58b871371ffcf7dddd69712094a8fc
Lawrence-AIP <liuch@allinfinance.com>
2024-04-23 10:52:46 +0800

    docs: 增加stream

--- using_files/test/test_llm.py
+++ using_files/test/test_llm.py
@@ -5,10 +5,11 @@ DEEPINFRA_API_KEY = os.getenv('DEEPINFRA_API_KEY')
 
 
 class ChatCompletion:
-    def __init__(self, temperature, model, api_key, base_url):
+    def __init__(self, temperature, model, api_key, base_url, stream=False):
         self.temperature = temperature
         self.model = model
         self.openai = OpenAI(api_key=api_key, base_url=base_url)
+        self.stream = stream
 
     def __call__(self, prompt):
         messages = [{"role": "user", "content": prompt}]
@@ -16,6 +17,7 @@ class ChatCompletion:
             model=self.model,
             messages=messages,
             temperature=self.temperature,
+            stream=self.stream,
         )
         return response
 
@@ -26,3 +28,10 @@ if __name__ == '__main__':
 
     response = llm("Hello")
     print(response.choices[0].message.content)
+
+    # llm2 = ChatCompletion(temperature=0.7, model="meta-llama/Meta-Llama-3-70B-Instruct",
+    #                       api_key=DEEPINFRA_API_KEY, base_url="https://api.deepinfra.com/v1/openai", stream=True)
+    #
+    # response2 = llm2("Hello")
+    # for event in response2:
+    #     print(event.choices[0].delta.content)
