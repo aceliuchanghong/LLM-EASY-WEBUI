bce47cc7b6acc7249e267e266bba85aeb5fc5795
lawrence <aceliuchanghong@gmail.com>
2024-05-21 22:09:25 +0800

    fix: rag搭建

--- using_files/test/testVecDB.py
+++ using_files/test/testVecDB.py
@@ -1,45 +1,33 @@
-import os
-import random
-# https://zhuanlan.zhihu.com/p/673500172
 from pymilvus import MilvusClient, DataType
-
-uri = 'https://in03-aa941e2a2cd2371.api.gcp-us-west1.zillizcloud.com'
-token = os.getenv("MILVUS_API_TOKEN")
+from chatAll import config
 
 client = MilvusClient(
-    uri=uri,
-    token=token
+    uri=config.MilvusClientUrl,
+    token=config.MILVUS_API_TOKEN
 )
 
-# 简易使用,之后不采取
-# client.create_collection(
-#     collection_name="quick_setup",
-#     dimension=5
-# )
+schema = MilvusClient.create_schema(
+    auto_id=False,
+    enable_dynamic_field=True,
+)
 
-# 采取
-# schema = MilvusClient.create_schema(
-#     auto_id=False,
-#     enable_dynamic_field=True,
-# )
-#
-# schema.add_field(field_name="my_id", datatype=DataType.INT64, is_primary=True)
-# schema.add_field(field_name="my_vector", datatype=DataType.FLOAT_VECTOR, dim=5)
-# schema.add_field(field_name="color", datatype=DataType.VARCHAR, max_length=200)
-#
-# index_params = client.prepare_index_params()
-#
-# index_params.add_index(
-#     field_name="my_vector",
-#     index_type="AUTOINDEX",
-#     metric_type="IP"
-# )
-#
-# client.create_collection(
-#     collection_name="customized_setup",
-#     schema=schema,
-#     index_params=index_params
-# )
+schema.add_field(field_name="my_id", datatype=DataType.INT64, is_primary=True)
+schema.add_field(field_name="my_vector", datatype=DataType.FLOAT_VECTOR, dim=5)
+schema.add_field(field_name="color", datatype=DataType.VARCHAR, max_length=200)
+
+index_params = client.prepare_index_params()
+
+index_params.add_index(
+    field_name="my_vector",
+    index_type="AUTOINDEX",
+    metric_type="COSINE"
+)
+
+client.create_collection(
+    collection_name="customized_setup",
+    schema=schema,
+    index_params=index_params
+)
 
 # 4.1. Prepare data
 data = [
